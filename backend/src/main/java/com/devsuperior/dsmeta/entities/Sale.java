58b5f6b5784cd66bf136fba3077648893215dddf
franklyn damaceno <frank-destro@outook.com>
2022-07-13 14:20:29 -0300

    Database

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tb_sales") 
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	private static SimpleDateFormat sdf = new  SimpleDateFormat("dd/MM/yyyy");
+
+	public Sale() {	
+	}
+	
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+	
+}
