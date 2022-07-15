83320b26fbc752477dfab595d8da8a70f439b9c6
franklyn damaceno <frank-destro@outook.com>
2022-07-15 16:00:51 -0300

    toast

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dsmeta-franklyn.herokuapp.com";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+
 
